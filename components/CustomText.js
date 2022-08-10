346531a94a61c3e54e3d066a15dfaaf2f70b381e
Krzysztof Podkulski <krzysztof453vip@gmail.com>
2022-08-10 15:30:16 +0200

    test

--- components/CustomText.js
+++ components/CustomText.js
@@ -1,22 +1,20 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-function CustomText({text,componentStyles}) {
+function CustomText = ({text,componentStyles}) => {
+    const CustomTextStyles = (styles)=>StyleSheet.create({
+        colorText: {
+            backgroundColor: '#fff',
+            alignItems: 'center',
+            justifyContent: 'center',
+            color: styles?.colorText?.color !== undefined ? styles.colorText.color : 'blue'
+          },
+     })
 
     const styles = CustomTextStyles(componentStyles)
 
-    return (
-        <Text style={styles.colorText}>{text}</Text>
-    );
+    return ( <Text style={styles.colorText}>{text}</Text> );
+    
 }
-
-const CustomTextStyles = (styles)=>StyleSheet.create({
-    colorText: {
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-        color: styles?.colorText?.color !== undefined ? styles.colorText.color : 'blue'
-      },
- })
 
 export default CustomText;
